abde7fd9f05cabc55606fb08fde1ed4ef0301636
olga <olga@flysky.fun>
2020-06-02 13:43:43 +0300

    создан интерфейс EventLogger

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,14 @@ public class App {
     private Client client;
     private ConsoleEventLogger eventLogger;
 
+    public App (Client client, ConsoleEventLogger eventLogger){
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
     public static void main(String[] args) {
-        App app = new App();
-        app.client = new Client("1", "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
-        app.logEvent("Some event for user 1");
+
+
     }
 
     private void logEvent(String msg){
