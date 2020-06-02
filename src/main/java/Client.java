bd4f85d994ee244d2a61aecbc836e4bbe7c36694
olga <olga@flysky.fun>
2020-06-02 11:44:38 +0300

    1 commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,20 @@
+public class Client {
+    String id;
+    String fullName;
+    public  Client(String id, String fullName){
+        this.id = id;
+        this.fullName = fullName;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+}
