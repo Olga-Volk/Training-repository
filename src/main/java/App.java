a945c5febf238f695f1cde028001148643d101f9
olga <olga@flysky.fun>
2020-06-02 14:20:29 +0300

    внесены изменения в spring.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     private Client client;
-    private ConsoleEventLogger eventLogger;
-
-    public App (Client client, ConsoleEventLogger eventLogger){
-        this.client = client;
-        this.eventLogger = eventLogger;
-    }
+    private EventLogger eventLogger;
 
     public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
 
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
+    }
 
+    public App (Client client, EventLogger eventLogger){
+        this.client = client;
+        this.eventLogger = eventLogger;
     }
 
     private void logEvent(String msg){
